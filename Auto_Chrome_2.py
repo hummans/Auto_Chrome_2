f4edf88cb1e4b830c86406414dee92bf61d3e291
Ali Raza <aliatwork@hotmail.co.uk>
2021-11-13 17:50:36 +0000

    Innitial comit

---
+++ Auto_Chrome_2.py
@@ -0,0 +1,193 @@
+import subprocess
+from tkinter import *
+from tkinter.messagebox import WARNING, showinfo
+from tkinter.filedialog import askopenfilename, asksaveasfilename
+import os
+import sys
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as ExpectedConditions
+from selenium.webdriver.common.keys import Keys
+import platform
+
+main_directory = os.path.join(sys.path[0])
+#start with new empty file
+file = None
+
+root = Tk()
+root.title("Chrome Automation")
+root.geometry("644x788")
+
+def open_chrome_profile():
+    os = platform.system()
+    if os == "Windows":
+        subprocess.Popen(
+            [
+                "start",
+                "chrome",
+                "--remote-debugging-port=8989",
+                "--user-data-dir=" + main_directory + "/chrome_profile",
+            ],
+            shell=True,
+        )
+    else:
+        subprocess.Popen([
+                "open -a /Applications/Google\ Chrome.app",
+                "--args",
+                "--remote-debugging-port=8989",
+                "--user-data-dir=" + main_directory + "/chrome_profile",
+            ],
+            shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    
+
+
+
+
+
+#####MENU BAR#####
+def newFile():
+    global file
+    root.title("Untitled - Auto_Chrome_2")
+    file = None
+    TextArea.delete(1.0, END)
+
+def openFile():
+    global file
+    file = askopenfilename(defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                     ("Text Documents", "*.txt")])
+    if file == "":
+        file = None
+    else:
+        root.title(os.path.basename(file) + " - Auto_Chrome_2")
+        TextArea.delete(1.0, END)
+        f = open(file, "r")
+        TextArea.insert(1.0, f.read())
+        f.close()
+
+def saveFile():
+    global file
+    if file == None:
+        file = asksaveasfilename(initialfile = 'Untitled.txt', defaultextension=".txt",
+                           filetypes=[("All Files", "*.*"),
+                                     ("Text Documents", "*.txt")])
+        if file =="":
+            file = None
+
+        else:
+            #Save as a new file
+            f = open(file, "w")
+            f.write(TextArea.get(1.0, END))
+            f.close()
+
+            root.title(os.path.basename(file) + " - Auto_Chrome_2")
+            print("File Saved")
+    else:
+        # Save the file
+        f = open(file, "w")
+        f.write(TextArea.get(1.0, END))
+        f.close()
+
+def quitApp():
+    root.destroy()
+def donate():
+    showinfo("Donate", "https://linktr.ee/Cloudmaking")
+def readme():
+    showinfo("ReadMe", "Comand List:\ngo_to(adress)\ncss_and_key(code, key)\nxpath_and_key(code, key)\ncss_and_click(code)\nxpath_and_click(code)")
+def about():
+    showinfo("Chrome Automator", "Auto_Chrome by @Cloudmaking")
+
+MenuBar = Menu(root)
+FileMenu = Menu(MenuBar, tearoff=0)
+FileMenu.add_command(label="New", command=newFile)
+FileMenu.add_command(label="Open", command = openFile)
+FileMenu.add_command(label = "Save", command = saveFile)
+FileMenu.add_separator()
+FileMenu.add_command(label = "Exit", command = quitApp)
+MenuBar.add_cascade(label = "File", menu=FileMenu)
+HelpMenu = Menu(MenuBar, tearoff=0)
+HelpMenu.add_command(label = "Donate", command=donate)
+HelpMenu.add_command(label = "ReadME", command=readme)
+HelpMenu.add_command(label = "About Notepad", command=about)
+MenuBar.add_cascade(label="Help", menu=HelpMenu)
+root.config(menu=MenuBar)
+#####MENU BAR END#####
+
+
+# _____MAIN_CODE_____
+def main_program_loop(): 
+
+    ##chromeoptions
+    opt = Options()
+    opt.add_experimental_option("debuggerAddress", "localhost:8989")
+    driver = webdriver.Chrome(
+        executable_path=main_directory + "/chromedriver.exe",
+        options=opt,
+        )    
+
+    wait = WebDriverWait(driver, 60)
+    #establish main page
+    main_page = driver.current_window_handle
+
+    def xpath_and_click(code):
+        x = wait.until(ExpectedConditions.presence_of_element_located((By.XPATH, code)))
+        x.click()
+
+    def css_and_click(code):
+        x = wait.until(ExpectedConditions.presence_of_element_located((By.CSS_SELECTOR, code)))
+        x.click()
+
+    def xpath_and_key(code, key):
+        x = wait.until(ExpectedConditions.presence_of_element_located((By.XPATH, code)))
+        x.send_keys(key)
+        time.sleep(1)
+
+    def css_and_key(code, key):
+        x = wait.until(ExpectedConditions.presence_of_element_located((By.CSS_SELECTOR, code)))
+        x.send_keys(key)
+        time.sleep(1)
+
+    def go_to(adress):
+        driver.get(adress)
+
+    exec(TextArea.get(1.0, END))
+
+
+####UI ELEMENTS######
+my_font = "Arial Black"
+
+info = Label(text = "Make sure the browser is open before starting or the program might freeze/crash", bg = "white")
+info.pack()
+info.config(font=("Arial", 9))
+
+open_browser = Button(text = "Open Browser", command = open_chrome_profile, bg = "yellow", font=(my_font, 10))
+open_browser.pack(anchor=N, pady=5, padx=5)
+
+start_button = Button(text = "Start", command = main_program_loop, bg = "green", font=(my_font, 10))
+start_button.pack(anchor=N, pady=5, padx=5)
+
+#number of loop entry
+e = Entry(root)
+e.insert(1, 1)
+e.label = Label(text= "Loop amount:")
+e.label.pack(anchor=W, pady=0, padx=1)
+e.config(font=(my_font, 8))
+e.label.config(font=(my_font, 8))
+e.pack(anchor=W, pady=1, padx=1)
+
+#Add TextArea
+TextArea = Text(root, font=("Arial", 10), bg = "light gray")
+TextArea.pack(expand=True, fill=BOTH, anchor=S)
+
+#scrooll bar
+Scroll = Scrollbar(TextArea)
+Scroll.pack(side=RIGHT,  fill=Y)
+Scroll.config(command=TextArea.yview)
+TextArea.config(yscrollcommand=Scroll.set)
+####UI ELEMENTS END####
+
+root.mainloop()
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # Auto_Chrome_2
  a cross platform pyhton IDE implimenting selenium 4
+
+
+### This is an open-source project. 
+If you want to support this project or me, please check out my NFTs and maybe buy some, i accept most bids.
+https://opensea.io/collection/cryptoverse-lone-wanderer
+or Donate below:
+https://paypal.me/CloudMaking?locale.x=en_GB
+
+---
+
+# INSTRUCTIONS
+1. Download Python and The Chrome browser (if you don’t have it already)
+2. Download and extract the project in you desired location (keep all files and folders that come with the repo in this folder)
+3. shift+Rigth click inside project folder and click "open powershell window here" 
+4. Pip install requirements.txt by running the following comand (pip install -r requirements.txt)
+5. Run the script (Auto_Chrome.py) by double clickiing it
+6. Open a preset file inside the script (make sure to pick the right preset for you or make your own)
+6. fill in the varible for your project uplaod properties, examples are provided (you can save for next time)
+7. Press the "Open Browser" button (login ot metamask and go to your collection)
+8. Run the script
+
+# PRESET BUILDER COMANDS (normal python code and syntax works)
+## Find CSS and XPATH codes through the inspect window on chrome
+### all base selenium code also works fine (this just maks it so you dont need to setup anything)
+1. go_to(adress)
+2. css_and_key(code, key)
+3. xpath_and_key(code, key)
+4. css_and_click(code)
+5. xpath_and_click(code)
+
+# Important Notes please read before starting: 
+1. Do not move anything in or out of the main script folder.
+2. (here is a link to a handy script which does that for you: https://github.com/FireMarshmallow/Easy-file-renamer)
+3. as far as know this only works with windows
+4. you might get a confirmation sign request form metamask on the first uplaod, you can manually click it and the script will carry on, if you click and press "start" again
+---
+
+If you have any questions or want to get in contact you can find me on instagram and twitter by searching @cloudmaking (feel free to DM).
